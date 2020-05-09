323386ad0139c79793f5c28f9c9efe3339d9de67
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-05-09 21:36:55 +0530

    added filter_void

--- array_void.c
+++ array_void.c
@@ -6,7 +6,27 @@ ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper){
   result->array = calloc(count, sizeof(Object));
   for (int i = 0; i < count; i++)
   {
-    result->array[i] = MapperVoid(src->array[i]);
+    result->array[i] = mapper(src->array[i]);
   }
+  return result;
+}
+
+ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate){
+   int count = src->length,index=0;
+  Object temp_result[count];
+  for (int i = 0; i < count; i++)
+  {
+    if (predicate(src->array[i])){
+      temp_result[index] = src->array[i];
+      index++;
+    }
+  }
+  ArrayVoid_ptr result = malloc(sizeof(ArrayVoid));
+  result->length = index;
+  result->array = calloc(index, sizeof(Object));
+  for (int i = 0; i < index; i++)
+  {
+    result->array[i] = temp_result[i];
+}
   return result;
 }
